2e4ac48df2c8e899b80f4a56c0d65d9c64f25cc0
Andreas Helbech Kleist <andreaskleist@gmail.com>
2023-12-06 14:04:46 +0100

    EMB-2014: media: intel/ipu4: Disable zlw_invalidate
    zlw_invalidate is not necessary to get the data through. It is unclear when/if it will be needed.
    
    It is removed here because it fails if called before the mmu has been setup, which
    happens in some code paths currently.
    
    Signed-off-by: Andreas Helbech Kleist <andreaskleist@gmail.com>

--- drivers/media/pci/intel/ipu6/ipu6.c
+++ drivers/media/pci/intel/ipu6/ipu6.c
@@ -147,7 +147,7 @@ static const struct ipu6_isys_internal_pdata ipu4_isys_ipdata = {
 						2, 2, 2, 2, 2, 2
 				},
 				.insert_read_before_invalidate = false,
-				.zlw_invalidate = true,
+				.zlw_invalidate = false,
 				.l1_stream_id_reg_offset =
 					IPU4_MMU_L1_STREAM_ID_REG_OFFSET,
 				.l2_stream_id_reg_offset =
@@ -204,7 +204,7 @@ static const struct ipu6_psys_internal_pdata ipu4_psys_ipdata = {
 						   2, 2, 2, 2, 2, 2
 				   },
 				   .insert_read_before_invalidate = false,
-				   .zlw_invalidate = true,
+				   .zlw_invalidate = false,
 				   .l1_stream_id_reg_offset =
 				   IPU4_MMU_L1_STREAM_ID_REG_OFFSET,
 				   .l2_stream_id_reg_offset =
@@ -241,7 +241,7 @@ static const struct ipu6_psys_internal_pdata ipu4_psys_ipdata = {
 						   2, 2, 2, 2, 2, 2
 				   },
 				   .insert_read_before_invalidate = false,
-				   .zlw_invalidate = true,
+				   .zlw_invalidate = false,
 				   .l1_stream_id_reg_offset =
 				   IPU4_MMU_L1_STREAM_ID_REG_OFFSET,
 				   .l2_stream_id_reg_offset =
