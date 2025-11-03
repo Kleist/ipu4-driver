f32dbc881cc889f80d56a751ecf8cb5644bf8b8c
Andreas Helbech Kleist <akle@ambu.com>
2025-11-03 21:26:39 +0100

    EMB-3274: ipu4: Include ipu4 regs files from ipu6 regs files
    This reduces the diff to upstream 6.10, and makes it simpler to keep the ipu6
    files up to date with upstream.

--- kernel/ipu4/ipu6.c
+++ kernel/ipu4/ipu6.c
@@ -36,8 +36,8 @@
 #include "ipu6-isys.h"
 #include "ipu6-mmu.h"
 #include "ipu6-platform-buttress-regs.h"
-#include "ipu4-platform-isys-csi2-reg.h"
-#include "ipu4-platform-regs.h"
+#include "ipu6-platform-isys-csi2-reg.h"
+#include "ipu6-platform-regs.h"
 
 #define IPU6_PCI_BAR		0
 
